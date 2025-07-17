454b32ec724d0c04603ee1b4b503737e6cb5a1c4
loading1031 <tjdans1031@gmail.com>
2025-07-17 17:47:27 +0900

    Chore: 피싱탐지 파일 실행 스크립트

---
+++ Model/test.sh
@@ -0,0 +1,6 @@
+# python run_dean_ENS.py --metric=euclidean \
+#                            --algo=BERT4ETH \
+#                            --model_index=bert4eth_1M_min3_dup_seq100_mask80_shared_zipfan5000_64000 \
+
+python run_phishing_detection.py  --algo=bert4eth \
+                           --model_index=bert4eth_1M_min3_dup_seq100_mask80_shared_zipfan5000_104000 \
